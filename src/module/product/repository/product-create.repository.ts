53a5d29f68b97ec7c66894ce1a9b58f42cd0f370
jinhwan7 <rkflql851@gmail.com>
2025-01-16 15:12:17 +0900

    mongoose로 바뀌거나 추가될 경우 예시

---
+++ src/module/product/repository/product-create.repository.ts
@@ -0,0 +1,12 @@
+//*****예시를 들기위해 작성했습니다*****
+
+// import { ProductEntity } from 'src/common/db/entity/product.entity';
+// import { ProductRepository } from './product.repository';
+
+// export class ProductCreateRepository implements ProductCreateOutboundPort {
+//   constructor(private readonly productRepository: ProductRepository) {}
+
+//   async execute(object: Record<string, any>): Promise<ProductEntity> {
+//     return await this.productRepository.create(object);
+//   }
+// }
---
+++ src/module/product/repository/product.repository.ts
@@ -0,0 +1,17 @@
+//*****예시를 들기위해 작성했습니다*****
+
+
+// import { Injectable } from '@nestjs/common';
+// import { UserEntity } from '../../../common/db/entity/user.entity';
+// import { MongoDbCustomRepository } from 'src/common/db/mongooseCustomRepository';
+
+
+
+// @Injectable()
+// export class ProductRepository extends MongoDbCustomRepository<ProductEntity> {
+//   constructor() {
+//     super();
+//   }
+// }
+
+
